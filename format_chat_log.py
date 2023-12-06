632aa94e40fa4b706d8bcbfeb96faf24d1e1049e
Bill Melvin <bill@billmelvin.com>
2023-12-05 21:07:20 -0500

    Handle chat.json format as of 5dec23. Markdown out

--- format_chat_log.py
+++ format_chat_log.py
@@ -1,29 +1,36 @@
 import json
 
 def format_chat_log(chat_log):
-    formatted_chat_log = ""
-    turns = chat_log['providerState']['turns']
-    for turn in turns:
-        request_message = turn['request']['message']
-        response_message = turn['response']['message']
-        formatted_chat_log += f"request: {request_message}\nresponse: {response_message}\n\n"
+    """
+    Formats the chat log in markdown format. 
+    Requests are prefixed with `> ` to format as block quotes.
+    The formatted chat log is returned as a string.
+    """
+    formatted_chat_log = "# From chat.json\n\n---\n\n"  
+    requests = chat_log['requests']
+    for request in requests:
+        request_text = request['message'].get('text')
+        responses = request['response']
+        response_text = ""
+        for response in responses:
+            response_value = response.get('value')
+            if response_value:
+                response_text += response_value + "\n"
+        formatted_chat_log += f"> {request_text}\n\n {response_text}\n\n---\n"
     return formatted_chat_log
 
 # Path to the JSON file
 file_path = 'chat.json'
-# Path to the output text file (newly created)
-output_file_path = 'formatted_chat_log.txt'
+
+# Path to the output markdown file (newly created)
+output_file_path = 'formatted_chat_log.md'
 
 # Reading the JSON file
 with open(file_path, 'r') as file:
     raw_data = file.read()
 
-# Finding the index where the relevant portion starts
-start_index = raw_data.find('"providerId": "copilot"')
-relevant_data = raw_data[start_index:]
-
-# Parsing the relevant JSON data
-chat_log = json.loads('{' + relevant_data)
+# Parsing the JSON data
+chat_log = json.loads(raw_data)
 
 # Formatting the chat log
 formatted_chat_log = format_chat_log(chat_log)
