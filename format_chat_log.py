c276c37043122010a865b1cc9faf8f5ea6a166dd
Todd Mitchell <toddmitchell@protonmail.com>
2024-01-16 10:49:07 -0600

    Update format_chat_log.py
    Changed default formatting to txt. Added header with requester/responder. Added to CLI arguments -html/-md for html/md instead of txt, -requests to omit responses, and -anon for omitting header.

--- format_chat_log.py
+++ format_chat_log.py
@@ -1,55 +1,78 @@
 from datetime import datetime
-
 import json
-import sys
+import argparse
+import html
 
-
-def format_chat_log(chat_log):
-    """
-    Formats the chat log in markdown format.
-    Requests are prefixed with `> ` to format as block quotes.
-    The formatted chat log is returned as a string.
-    """
-    formatted_chat_log = "# From chat.json\n\n---\n\n"
-    requests = chat_log['requests']
-    for request in requests:
-        request_text = request['message'].get('text')
-        responses = request['response']
-        response_text = ""
-        for response in responses:
-            response_value = response.get('value')
-            if response_value:
-                response_text += response_value + "\n"
-        formatted_chat_log += f"> {request_text}\n\n {response_text}\n\n---\n"
-    return formatted_chat_log
-
+def format_chat_log(chat_log, format='txt', requests_only=False, anon=False):
+	formatted_chat_log = ""
+	requests = chat_log['requests']
+	if not anon:
+		requester_username = chat_log.get('requesterUsername', '')
+		responder_username = chat_log.get('responderUsername', '')
+		if format == 'html':
+			formatted_chat_log += f"<p>Coder: {html.escape(requester_username)}</p>\n<p>AI: {html.escape(responder_username)}</p>\n<hr>\n"
+		else:
+			formatted_chat_log += f"Coder: {requester_username}\nAI: {responder_username}\n\n"
+	if requests_only and format == 'html':
+		formatted_chat_log += "<ul>\n"
+	for request in requests:
+		request_text = request['message'].get('text') if format == 'txt' else "> " + html.escape(request['message'].get('text'))
+		if not requests_only:
+			responses = request['response']
+			response_text = ""
+			for response in responses:
+				response_value = response.get('value')
+				if response_value:
+					response_text += (response_value if format == 'txt' else html.escape(response_value)) + "\n"
+			if format == 'md':
+				formatted_chat_log += f"> {request_text}\n\n {response_text}\n\n---\n"
+			elif format == 'html':
+				formatted_chat_log += f"<p>{request_text}</p>\n<p>{response_text}</p>\n<hr>\n"
+			else:
+				formatted_chat_log += f"> {request_text}\n\n{response_text}\n"
+		else:
+			if format == 'md':
+				formatted_chat_log += f"> {request_text}\n\n---\n"
+			elif format == 'html':
+				formatted_chat_log += f"<li>{request_text}</li>\n"
+			else:
+				formatted_chat_log += f"> {request_text}\n"
+	if requests_only and format == 'html':
+		formatted_chat_log += "</ul>\n"
+	return formatted_chat_log
 
 def main():
-    # Path to the JSON file
-    if len(sys.argv) == 2:
-        file_path = sys.argv[1]
-    else:
-        file_path = "chat.json"
+	parser = argparse.ArgumentParser(description='Format a chat log.')
+	parser.add_argument('file_path', metavar='F', type=str, nargs='?', default='chat.json',
+						help='the path to the chat log file')
+	parser.add_argument('-md', action='store_true',
+						help='format the chat log in markdown instead of txt')
+	parser.add_argument('-html', action='store_true',
+						help='format the chat log in html instead of txt')
+	parser.add_argument('-requests', action='store_true',
+						help='only output requests, not responses')
+	parser.add_argument('-anon', action='store_true',
+						help='omit requester and responder usernames')
+	args = parser.parse_args()
 
-    # Path to the output markdown file (newly created)
-    output_file_path = (
-        f'formatted_chat_log-{datetime.now().strftime("%Y%m%d_%H%M%S")}.md'
-    )
+	file_path = args.file_path
+	format = 'md' if args.md else 'html' if args.html else 'txt'
+	requests_only = args.requests
+	anon = args.anon
 
-    # Reading the JSON file
-    with open(file_path, 'r') as file:
-        raw_data = file.read()
+	output_file_path = (
+		f'formatted_chat_log-{datetime.now().strftime("%Y%m%d_%H%M%S")}.{format}'
+	)
 
-    # Parsing the JSON data
-    chat_log = json.loads(raw_data)
+	with open(file_path, 'r') as file:
+		raw_data = file.read()
 
-    # Formatting the chat log
-    formatted_chat_log = format_chat_log(chat_log)
+	chat_log = json.loads(raw_data)
 
-    # Creating and saving the formatted chat log to a new text file
-    with open(output_file_path, 'w') as file:
-        file.write(formatted_chat_log)
+	formatted_chat_log = format_chat_log(chat_log, format, requests_only, anon)
 
+	with open(output_file_path, 'w') as file:
+		file.write(formatted_chat_log)
 
 if __name__ == '__main__':
-    main()
+	main()
